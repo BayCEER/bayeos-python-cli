f9e3d4269a190ac9b719bb3d2b63d4e4038251b3
oarchner <oliver.archner@uni-bayreuth.de>
2015-03-04 13:29:02 +0100

    Removed zip_saved=False

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+	from distutils.core import setup 
 
 setup(name='bayeos-python-cli',
-      version='0.1',
+      version='1.0',
       description='A simple bayeoes-server client',
       url='http://github.com/BayCEER/bayeos-python-cli',
       author='Oliver Archner',
       author_email='oliver.archner@uni-bayreuth.de',
       license='GPL2',
       packages=['bayeos'],
-      package_dir = {'': 'src'},
-      zip_safe=False)
+      package_dir = {'': 'src'})
 
