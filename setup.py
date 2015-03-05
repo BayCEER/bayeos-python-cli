81f29fd133fdacce31116789b42231b142357682
oarchner <oliver.archner@uni-bayreuth.de>
2015-03-05 10:04:30 +0100

    Version 1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
 	from distutils.core import setup 
 
 setup(name='bayeos-python-cli',
-      version='1.0',
+      version='1.1',
       description='A simple bayeoes-server client',
       url='http://github.com/BayCEER/bayeos-python-cli',
       author='Oliver Archner',
