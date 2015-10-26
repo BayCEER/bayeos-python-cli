8b48a69f2f1e86d509335bff6407314a5b4c80b4
oarchner <oliver.archner@uni-bayreuth.de>
2015-10-26 17:08:44 +0100

    Adapted python version

--- PlatformTest.bat
+++ PlatformTest.bat
@@ -1,5 +1,5 @@
 set PY3=C:\Python34
-set PY2=C:\Python278
+set PY2=C:\Python279
 
 SET PATH=%PY3%
 python setup.py install 
@@ -8,7 +8,6 @@ python tests\bayeos\CliTest.py
 SET PATH=%PY2%
 python setup.py install 
 python tests\bayeos\CliTest.py
-
 
 
 
