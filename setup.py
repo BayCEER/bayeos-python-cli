c478c8c0f6d32b5955ce327bc6d57b183dc8631e
oarchner <oliver.archner@uni-bayreuth.de>
2015-11-20 11:42:35 +0100

    Added import scripts

--- setup.py
+++ setup.py
@@ -4,22 +4,31 @@ except ImportError:
 	from distutils.core import setup 
 
 setup(name='bayeos-python-cli',
-      version='1.2',
+      version='1.3',
       description='A simple bayeoes-server client',
       url='http://github.com/BayCEER/bayeos-python-cli',
       author='Oliver Archner',
       author_email='oliver.archner@uni-bayreuth.de',
       license='GPL2',
       packages=['bayeos'],
-      package_dir = {'': 'src'})
+      package_dir = {'': 'src'},
+      install_requires=[
+        'pytz'
+      ])
 
 
 setup(name='bayeos-python-frame',
-      version='1.2',
+      version='1.3',
       description='A bayeoes-server frame client',
       url='http://github.com/BayCEER/bayeos-python-frame',
       author='Oliver Archner',
       author_email='oliver.archner@uni-bayreuth.de',
       license='GPL2',
-      packages=['bayeos', 'bayeos.frame', ],
-      package_dir = {'': 'src'})
+      packages=['bayeos.frame', ],
+      package_dir = {'': 'src'},
+      install_requires=[
+        'pytz',
+        'numpy',
+        'pandas'
+      ]
+      )
